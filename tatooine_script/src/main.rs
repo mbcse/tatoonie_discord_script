52f78a027899e22f4541b9518961daf7f26f81a8
mbcse <mbcse50@gmail.com>
2021-08-22 18:52:10 +0530

    Added Get Balance Update function

--- tatooine_script/src/main.rs
+++ tatooine_script/src/main.rs
@@ -1,21 +1,21 @@
 extern crate reqwest;
 use std::collections::HashMap;
 
-fn send_discord_update(url:&str){
+fn send_discord_message(discord_url:&str,msg:&str){
     let client = reqwest::blocking::Client::new();
 
     let mut body = HashMap::new();
-    body.insert("username", "tatoonie_script");
-    body.insert("content", "Faucet Server Is Down");
+    body.insert("username", "Tatooine_Script");
+    body.insert("content", msg);
 
-    let resp = client.post(url)
+    let resp = client.post(discord_url)
     .json(&body)
     .send();
 
     match resp {
         Ok(res) => {
             if res.status()==204 {
-                println!("Server Down Message Sent!");
+                println!("Message Sent!");
             }else {
                 println!("Problem while Sending Message!");
             }
@@ -24,6 +24,46 @@ fn send_discord_update(url:&str){
             println!("Problem while Sending Message!");
         }
     } 
+
+}
+
+fn send_serverstatus_update(faucet_url:&str,discord_url:&str){
+    let response = reqwest::blocking::get(faucet_url);
+    match response {
+       Ok(res)=> {
+         if res.status()==reqwest::StatusCode::OK{
+             println!("Faucet Server Is Working Fine : {:?}",res.text().unwrap()); 
+         }else{
+             send_discord_message(discord_url,"Faucet Server Down!");
+         }  
+       },
+       Err(..)=> {
+           send_discord_message(discord_url,"Faucet Server Down!");
+       }
+    }
+
+}
+
+
+fn send_balance_update(faucet_url:&str,discord_url:&str,password:&str){
+    let response = reqwest::blocking::Client::new()
+    .get(faucet_url.to_owned()+"/getbalance")
+    .basic_auth("discord_script",Some(password)).send();
+
+    match response {
+       Ok(res)=> {
+         if res.status()==reqwest::StatusCode::OK{
+             let balance:&str=&res.text().unwrap();
+             println!("{:?}",balance);
+             send_discord_message(discord_url,balance);
+         }else{
+             send_discord_message(discord_url,"Can't get the balance, Faucet server may be down!");
+         }  
+       },
+       Err(..)=> {
+           send_discord_message(discord_url,"Can't get the balance, Faucet server may be down!");
+       }
+    }
 
 }
 
@@ -36,19 +76,10 @@ fn main() {
 
    let faucet_url: &str=conf.get("FAUCET_URL").unwrap();
    let discord_webhook_url: &str=conf.get("DISCORD_WEBHOOK_URL").unwrap();
+   let password: &str=conf.get("PASSWORD").unwrap();
 
-   let response = reqwest::blocking::get(faucet_url);
-   match response {
-      Ok(res)=> {
-        if res.status()==reqwest::StatusCode::OK{
-            println!("Faucet Server Working Fine {:?}",res.text()); 
-        }else{
-            send_discord_update(discord_webhook_url);
-        }  
-      },
-      Err(..)=> {
-          send_discord_update(discord_webhook_url);
-      }
-   }
+   send_serverstatus_update(faucet_url,discord_webhook_url);
+   send_balance_update(faucet_url,discord_webhook_url,password);
+  
 
 }
